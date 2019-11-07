a5e122918c5b918319ec61e2715e9523ff7296c7
deissyMantilla <deissymantilla@outlook.com>
2019-11-07 09:53:17 -0500

    Se configuran rutas del CRUD

--- Backend/routes/usuarios.js
+++ Backend/routes/usuarios.js
@@ -2,7 +2,7 @@
 
 const express = require("express");
 const usuariosRoute = express.Router();
-const Usuarios = require("../modelos/usuarios");
+const Usuarios = require("../models/usuarios");
 
 //POST
 usuariosRoute.post("/usuarios", (req, res, next) => {
@@ -12,3 +12,39 @@ usuariosRoute.post("/usuarios", (req, res, next) => {
     })
     .catch(next);
 });
+
+//GET
+
+usuariosRoute.get("/usuarios/:email", (req, res, next) => {
+  Usuarios.findOne({ email: req.params.email }, (err, usuarioExistente) => {
+    if (usuarioExistente !== null) {
+      Usuarios.findOne({ email: req.params.email }, req.body)
+        .then(() => {
+          const usuarios = Usuarios.findOne({ email: req.params.email });
+          return usuarios;
+        })
+        .then(usuarios => {
+          res.status(200).send(usuarios);
+        })
+        .catch(next);
+    } else {
+      res.json(null);
+    }
+  });
+});
+
+//PUT
+usuariosRoute.put("/usuarios/:email", (req, res, next) => {
+  Usuarios.findOneAndUpdate({ email: req.params.email }, req.body)
+    .then(() => {
+      const usuarios = Usuarios.findOne({ email: req.params.email });
+      return usuarios;
+    })
+    .then(usuarios => {
+      res.send(usuarios);
+    })
+    .catch(next);
+});
+
+//LLAMANDO
+module.exports = usuariosRoute;
