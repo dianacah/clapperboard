d1f5deb1f40bc5b7a6bbad15a1d37e847173ae20
deissyMantilla <deissymantilla@outlook.com>
2019-11-08 17:05:05 -0500

    Se crea popup para mostrar la informacion de la pelicula

--- frontend/src/app/peliculas/peliculas.component.ts
+++ frontend/src/app/peliculas/peliculas.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from "@angular/core";
+import {
+  MatDialog,
+  MatDialogConfig,
+  MatTableDataSource
+} from "@angular/material";
+import { PopupInfoPeliComponent } from "./../popup-info-peli/popup-info-peli.component";
 
 @Component({
   selector: "app-peliculas",
@@ -9,27 +15,91 @@ export class PeliculasComponent implements OnInit {
   public generos = ["Drama", "Comedia", "Romance", "Anime"];
   public drama = [
     {
-      nombre: " A los trece",
-      imagen: "../../assets/home/images/Drama/ALosTrece.jpg"
+      title: " A los trece",
+      director: "Christian",
+      duration: "2:00",
+      genre: "Drama",
+      actors: "lllll, kkkkkk",
+      file: "./../",
+      image: "../../assets/home/images/Drama/ALosTrece.jpg",
+      synopsis: "Lorem ipsum lorem ipsum",
+      trailer: "./../../"
     },
     {
-      nombre: " A los trece",
-      imagen: "../../assets/home/images/Drama/ALosTrece.jpg"
+      title: " A los trece",
+      director: "Christian",
+      duration: "2:00",
+      genre: "Drama",
+      actors: "lllll, kkkkkk",
+      file: "./../",
+      image: "../../assets/home/images/Drama/ALosTrece.jpg",
+      synopsis: "Lorem ipsum lorem ipsum",
+      trailer: "./../../"
     },
     {
-      nombre: " A los trece",
-      imagen: "../../assets/home/images/Drama/ALosTrece.jpg"
+      title: " A los trece",
+      director: "Christian",
+      duration: "2:00",
+      genre: "Drama",
+      actors: "lllll, kkkkkk",
+      file: "./../",
+      image: "../../assets/home/images/Drama/ALosTrece.jpg",
+      synopsis: "Lorem ipsum lorem ipsum",
+      trailer: "./../../"
     },
     {
-      nombre: " A los trece",
-      imagen: "../../assets/home/images/Drama/ALosTrece.jpg"
+      title: " A los trece",
+      director: "Christian",
+      duration: "2:00",
+      genre: "Drama",
+      actors: "lllll, kkkkkk",
+      file: "./../",
+      image: "../../assets/home/images/Drama/ALosTrece.jpg",
+      synopsis: "Lorem ipsum lorem ipsum",
+      trailer: "./../../"
     },
     {
-      nombre: " A los trece",
-      imagen: "../../assets/home/images/Drama/ALosTrece.jpg"
+      title: " A los trece",
+      director: "Christian",
+      duration: "2:00",
+      genre: "Drama",
+      actors: "lllll, kkkkkk",
+      file: "./../",
+      image: "../../assets/home/images/Drama/ALosTrece.jpg",
+      synopsis: "Lorem ipsum lorem ipsum",
+      trailer: "./../../"
     }
   ];
-  constructor() {}
+  public popup;
+  constructor(private dialog: MatDialog) {}
+
+  openDialog() {
+    const dialogConfig = new MatDialogConfig();
+    dialogConfig.disableClose = false;
+    dialogConfig.autoFocus = false;
+    dialogConfig.width = "400px";
+    dialogConfig.height = "400px";
+    return dialogConfig;
+  }
+
+  mostrarPelicula(pelicula) {
+    let dialogConfig = this.openDialog();
+    dialogConfig.data = {
+      title: pelicula.title,
+      director: pelicula.director,
+      duration: pelicula.duration,
+      genre: pelicula.genre,
+      actors: pelicula.actors,
+      file: pelicula.file,
+      image: pelicula.image,
+      synopsis: pelicula.synopsis,
+      trailer: pelicula.trailer
+    };
+    this.popup = this.dialog.open(PopupInfoPeliComponent, dialogConfig);
+    this.popup.afterClosed().subscribe(response => {
+      let respuesta = response.value;
+    });
+  }
 
   ngOnInit() {}
 }
--- frontend/src/app/popup-info-peli/popup-info-peli.component.ts
+++ frontend/src/app/popup-info-peli/popup-info-peli.component.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject } from "@angular/core";
+import { MAT_DIALOG_DATA, MatDialogRef } from "@angular/material";
 
 @Component({
-  selector: 'app-popup-info-peli',
-  templateUrl: './popup-info-peli.component.html',
-  styleUrls: ['./popup-info-peli.component.css']
+  selector: "app-popup-info-peli",
+  templateUrl: "./popup-info-peli.component.html",
+  styleUrls: ["./popup-info-peli.component.css"]
 })
 export class PopupInfoPeliComponent implements OnInit {
+  public movie: string;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(
+    private dialogRef: MatDialogRef<PopupInfoPeliComponent>,
+    @Inject(MAT_DIALOG_DATA) data
+  ) {
+    this.movie = data;
   }
 
+  ngOnInit() {
+    console.log(this.movie);
+  }
 }
