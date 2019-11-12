9ec9c5b6dbf9fe0b266e8aee96eb02fc6e0dd292
GeraldineCabrera <geraldinecaby@gmail.com>
2019-11-12 11:29:36 -0500

    Se configuran las rutas para poder usar las anclas

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -4,7 +4,7 @@ import { PopupInfoPeliComponent } from "./popup-info-peli/popup-info-peli.compon
 import { PopupNewFavComponent } from "./popup-new-fav/popup-new-fav.component";
 
 import { GenComponent } from "./gen/gen.component";
-import { RouterModule, Routes, ExtraOptions } from "@angular/router";
+import { RouterModule, Routes } from "@angular/router";
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 
@@ -37,11 +37,6 @@ import { ReleasesComponent } from "./releases/releases.component";
 import { ReactiveFormsModule } from "@angular/forms";
 import { NavGendersComponent } from "./nav-genders/nav-genders.component";
 
-const routerOptions: ExtraOptions = {
-  scrollPositionRestoration: 'enabled',
-  anchorScrolling: 'enabled',
-  scrollOffset: [0, 64],
-};
 
 
 const routes: Routes = [
@@ -112,7 +107,7 @@ const routes: Routes = [
     MatPaginatorModule,
     MatProgressSpinnerModule,
     MatSortModule,
-    RouterModule.forRoot(routes, routerOptions),
+    RouterModule.forRoot(routes, {useHash: true}),
     MatTabsModule,
     BrowserAnimationsModule,
     MatIconModule,
