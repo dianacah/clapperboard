fb5d3784e55427b29b6774b792e3d6725a8c8832
deissyMantilla <deissymantilla@outlook.com>
2019-11-13 18:33:10 -0500

    validacion de mayor de edad

--- frontend/src/app/authentication/signup/signup.component.ts
+++ frontend/src/app/authentication/signup/signup.component.ts
@@ -11,7 +11,8 @@ import { Router } from "@angular/router";
 })
 export class SignupComponent implements OnInit {
   guardarInformacion: any = {};
-
+  public fechaActual: Date = new Date();
+  public menorEdad: boolean = false;
   constructor(
     private builder: FormBuilder,
     private userInformationService: UserInformationService,
@@ -30,13 +31,30 @@ export class SignupComponent implements OnInit {
   });
 
   enviarInformacion(signupForm) {
+    console.log(this.fechaActual);
+    let fechaRegistro = signupForm.value.date;
+    let añoRegistro = parseInt(fechaRegistro.substr(0, 4));
+    let mesRegistro = parseInt(fechaRegistro.substr(5, 2));
+    let diaRegistro = parseInt(fechaRegistro.substr(8, 2));
+    let añoActual = this.fechaActual.getFullYear();
+    let mesActual = this.fechaActual.getMonth();
+    let diaActual = this.fechaActual.getDay();
+    let años = añoActual - añoRegistro;
+    let meses = mesActual - mesRegistro;
+    let dia = diaActual - diaRegistro;
+    console.log("año", años, "mes", meses, "dia", dia);
     let usuarioNuevo = signupForm.value;
-    this.registryUserService
-      .postRegistry(usuarioNuevo)
-      .subscribe((response = {}) => {
-        this.guardarInformacion = response;
-        this.userInformationService.setUser(this.guardarInformacion);
-      });
+    if (años >= 18) {
+      this.registryUserService
+        .postRegistry(usuarioNuevo)
+        .subscribe((response = {}) => {
+          this.guardarInformacion = response;
+          this.userInformationService.setUser(this.guardarInformacion);
+          this.router.navigate(["/perfil"]);
+        });
+    } else {
+      this.menorEdad = true;
+    }
   }
 
   ngOnInit() {}
